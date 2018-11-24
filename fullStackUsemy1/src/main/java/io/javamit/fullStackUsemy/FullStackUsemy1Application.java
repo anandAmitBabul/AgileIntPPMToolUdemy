81f31e01111fffd65cd1bb52df021df67bd9f864
Amit Anand <amit.baabul.anand@gmail.com>
2018-11-24 17:43:35 +0530

    first commit

---
+++ fullStackUsemy1/src/main/java/io/javamit/fullStackUsemy/FullStackUsemy1Application.java
@@ -0,0 +1,12 @@
+package io.javamit.fullStackUsemy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackUsemy1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStackUsemy1Application.class, args);
+	}
+}
---
+++ fullStackUsemy1/src/main/java/io/javamit/fullStackUsemy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package io.javamit.fullStackUsemy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(FullStackUsemy1Application.class);
+	}
+
+}
